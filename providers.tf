7f531a60a466650d4d7381642a49041f1b1854a7
Eduardo Santos <edu.nascimento22@outlook.com>
2026-09-15 10:09:07 -0300

    feat: correcao rds

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.9.0"
 
   backend "s3" {
-    bucket         = "techchallenge-tfstate"
-    key            = "database/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "techchallenge-tf-locks"
-    encrypt        = true
+    bucket       = "techchallenge-tfstate-477478162709"
+    key          = "database/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 
   required_providers {
